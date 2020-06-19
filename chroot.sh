7fabbdeb0f6f2cfd6bc6fa959bd356e0b08dfb8e
vardas <johnvardas@hotmail.com>
2020-06-19 05:46:28 +0300

    Chroot file with mounts and stuff

---
+++ chroot.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cp --dereference /etc/resolv.conf /mnt/gentoo/etc/
+mount --types proc /proc /mnt/gentoo/proc
+mount --rbind /sys /mnt/gentoo/sys
+mount --make-rslave /mnt/gentoo/sys
+mount --rbind /dev /mnt/gentoo/dev
+mount --make-rslave /mnt/gentoo/dev
+chroot /mnt/gentoo /bin/bash
+source /etc/profile
+export PS1="(chroot) ${PS1}"
+mount /dev/sda2 /boot
